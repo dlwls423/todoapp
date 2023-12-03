e41b76a85884c60db0eb8a5e5e62aeeb1181aed4
LeeYejin <leeyejin113@gmail.com>
2023-12-03 14:13:19 +0900

    refactor: comment service test

--- src/test/java/com/sparta/todoapp/service/CommentServiceTest.java
+++ src/test/java/com/sparta/todoapp/service/CommentServiceTest.java
@@ -7,19 +7,17 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 import com.sparta.todoapp.controller.exception.AuthorizeException;
-import com.sparta.todoapp.controller.exception.BadAccessToCardException;
 import com.sparta.todoapp.dto.CommentRequestDto;
 import com.sparta.todoapp.dto.CommentResponseDto;
 import com.sparta.todoapp.entity.Card;
 import com.sparta.todoapp.entity.Comment;
 import com.sparta.todoapp.entity.User;
-import com.sparta.todoapp.repository.CardRepository;
 import com.sparta.todoapp.repository.CommentRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -34,17 +32,26 @@ class CommentServiceTest {
     @Mock
     CardService cardService;
 
+    CommentService commentService;
+
+    User user;
+    Card card;
+    CommentRequestDto requestDto;
+    Comment comment;
+
+    @BeforeEach
+    void setup() {
+        commentService = new CommentService(commentRepository, cardService);
+        user = new User("lucy", "1234");
+        card = new Card("제목", "내용", false, false, user);
+        requestDto = new CommentRequestDto("내용");
+        comment = new Comment(requestDto, user, card);
+    }
+
     @Test
     @DisplayName("댓글 생성")
     void test1() {
         // given
-        CommentService commentService = new CommentService(commentRepository, cardService);
-        CommentRequestDto requestDto = new CommentRequestDto("내용");
-        User user = new User("lucy", "1234");
-        Card card = new Card("제목", "내용", false, false, user);
-        Comment comment = new Comment(requestDto, user, card);
-        comment.updateContent("댓글 내용");
-        CommentResponseDto responseDto = new CommentResponseDto(comment);
         given(cardService.getCardEntity(1L)).willReturn(card);
         when(commentRepository.save(any(Comment.class))).thenReturn(comment);
 
@@ -52,7 +59,7 @@ class CommentServiceTest {
         CommentResponseDto resultResponseDto = commentService.createComment(1L, requestDto, user);
 
         // then
-        assertEquals(responseDto.getContent(), resultResponseDto.getContent());
+        assertEquals(requestDto.getContent(), resultResponseDto.getContent());
 
     }
 
@@ -60,9 +67,6 @@ class CommentServiceTest {
     @DisplayName("특정 카드의 댓글 조회")
     void test2() {
         // given
-        CommentService commentService = new CommentService(commentRepository, cardService);
-        User user = new User("lucy", "1234");
-        Card card = new Card("제목", "내용", false, false, user);
         Comment comment1 = new Comment("댓글1", user, card);
         Comment comment2 = new Comment("댓글2", user, card);
         Comment comment3 = new Comment("댓글3", user, card);
@@ -85,17 +89,13 @@ class CommentServiceTest {
     @DisplayName("댓글 수정")
     void test3() {
         // given
-        CommentService commentService = new CommentService(commentRepository, cardService);
-        User user = new User("lucy", "1234");
-        Card card = new Card("제목", "내용", false, false, user);
-        Comment comment = new Comment("댓글", user, card);
-        CommentRequestDto requestDto = new CommentRequestDto("댓글 수정");
+        CommentRequestDto updateRequestDto = new CommentRequestDto("댓글 수정");
 
         given(cardService.getCardEntity(1L)).willReturn(card);
         given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
 
         // when
-        CommentResponseDto responseDto = commentService.updateComment(1L, 1L, requestDto, user);
+        CommentResponseDto responseDto = commentService.updateComment(1L, 1L, updateRequestDto, user);
 
         // then
         assertEquals("댓글 수정", responseDto.getContent());
@@ -105,19 +105,16 @@ class CommentServiceTest {
     @DisplayName("댓글 수정 - 해당 카드의 댓글이 아닌 경우")
     void test4() {
         // given
-        CommentService commentService = new CommentService(commentRepository, cardService);
-        User user = new User("lucy", "1234");
-        Card card1 = new Card("제목", "내용", false, false, user);
         Card card2 = new Card("제목", "내용", false, false, user);
         Comment comment = new Comment("댓글", user, card2);
-        CommentRequestDto requestDto = new CommentRequestDto("댓글 수정");
+        CommentRequestDto updateRequestDto = new CommentRequestDto("댓글 수정");
 
-        given(cardService.getCardEntity(1L)).willReturn(card1);
+        given(cardService.getCardEntity(1L)).willReturn(card);
         given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
 
         // when
         AuthorizeException exception = assertThrows(AuthorizeException.class, () ->
-            commentService.updateComment(1L, 1L, requestDto, user)
+            commentService.updateComment(1L, 1L, updateRequestDto, user)
         );
 
         // then
@@ -128,19 +125,15 @@ class CommentServiceTest {
     @DisplayName("댓글 수정 - 작성자가 아닌 경우")
     void test5() {
         // given
-        CommentService commentService = new CommentService(commentRepository, cardService);
-        User user1 = new User("lucy1", "1234");
         User user2 = new User("lucy2", "1234");
-        Card card = new Card("제목", "내용", false, false, user1);
-        Comment comment = new Comment("댓글", user1, card);
-        CommentRequestDto requestDto = new CommentRequestDto("댓글 수정");
+        CommentRequestDto updateRequestDto = new CommentRequestDto("댓글 수정");
 
         given(cardService.getCardEntity(1L)).willReturn(card);
         given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
 
         // when
         AuthorizeException exception = assertThrows(AuthorizeException.class, () ->
-            commentService.updateComment(1L, 1L, requestDto, user2)
+            commentService.updateComment(1L, 1L, updateRequestDto, user2)
         );
 
         // then
